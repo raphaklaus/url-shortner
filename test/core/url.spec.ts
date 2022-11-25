51cae9c19d2201ea07a2e12ef279c3ff485e4a38
Raphael D. Pinheiro <raphaklaus@gmail.com>
2022-11-25 13:48:35 +0100

    Add URL Shortner first spec

---
+++ test/core/url.spec.ts
@@ -0,0 +1,18 @@
+import AlphanumericStrategy from "../../src/core/alpha.strategy";
+import URLShortner from "../../src/core/urlShortner.core";
+
+describe("URL Shortner", () => {
+  test("should return a valid shortned URL", async () => {
+    const strategy = new AlphanumericStrategy("tier.app");
+    const urlShortner = new URLShortner(strategy);
+    const url = await urlShortner.process("https://google.com");
+    const parsedUrl = new URL(url.to);
+
+    expect(parsedUrl).toMatchObject({
+      protocol: "https:",
+      host: "tier.app",
+    });
+
+    expect(parsedUrl.pathname.length).toBeGreaterThan(1);
+  });
+});
