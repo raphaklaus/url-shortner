ce2bfc38a9c33b94e06f7142ea07e338abaabbac
Raphael D. Pinheiro <raphaklaus@gmail.com>
2022-11-26 13:35:08 +0100

    Add update method

--- src/repository/repository.interface.ts
+++ src/repository/repository.interface.ts
@@ -1,4 +1,5 @@
 export default interface IRepository {
   create(data: any): Promise<any>;
   findUnique(query: { where: any }): Promise<any>;
+  update(opts: { data: any; where: any }): Promise<any>;
 }
