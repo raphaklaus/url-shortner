df4e99725937bb97c04522c58a71739256cfffba
Raphael D. Pinheiro <raphaklaus@gmail.com>
2022-11-26 13:37:19 +0100

    Control access and small refactorings

--- src/services/urlShortner.service.ts
+++ src/services/urlShortner.service.ts
@@ -1,26 +1,35 @@
 import { IStrategy } from "../core/strategy.interface";
 import URLShortner from "../core/urlShortner.core";
 import IRepository from "../repository/repository.interface";
+import { extractCode } from "./utils/url";
 
 export default class URLShortnerService {
-  strategy: IStrategy;
-  shortner: URLShortner;
-  shortURLRepository: IRepository;
-  constructor(strategy: IStrategy, shortURLRepository: IRepository) {
-    this.strategy = strategy;
+  private readonly shortner: URLShortner;
+  private readonly shortURLRepository: IRepository;
+  private readonly baseDomain: string;
+  constructor(
+    strategy: IStrategy,
+    shortURLRepository: IRepository,
+    baseDomain = "tier.app"
+  ) {
+    this.baseDomain = baseDomain;
     this.shortURLRepository = shortURLRepository;
     this.shortner = new URLShortner(strategy);
   }
 
-  create(url: string): Promise<any> | undefined {
+  async create(url: string): Promise<string> {
     const result = this.shortner.process(url);
 
-    return this.shortURLRepository.create({
-      data: { source: result.from, code: result.to },
+    const shortURL = await this.shortURLRepository.create({
+      data: { source: result.from, code: result.to, visit: { create: {} } },
     });
+
+    return `https://${this.baseDomain}/${shortURL.code}`;
   }
 
-  get(code: string): Promise<any> {
-    return this.shortURLRepository.findUnique({ where: { code } });
+  get(url: string): Promise<any> {
+    return this.shortURLRepository.findUnique({
+      where: { code: extractCode(url) },
+    });
   }
 }
