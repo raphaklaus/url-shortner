83e3c54fc923cea2215f796ecdc0d125ee3b92c4
Raphael D. Pinheiro <raphaklaus@gmail.com>
2022-11-27 16:52:16 +0100

    Remove repository

--- src/repository/repository.interface.ts
+++
@@ -1,5 +0,0 @@
-export default interface IRepository {
-  create(data: any): Promise<any>;
-  findUnique(query: { where: any }): Promise<any>;
-  update(opts: { data: any; where: any }): Promise<any>;
-}
