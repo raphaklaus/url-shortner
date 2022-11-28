b398340ac7d273be0ce9e0e4cc8a2bb34eb1bead
Raphael D. Pinheiro <raphaklaus@gmail.com>
2022-11-28 13:18:35 +0100

    Propagate utility into tests

--- test/services/url.service.spec.ts
+++ test/services/url.service.spec.ts
@@ -1,10 +1,11 @@
-import { AppDataSource, startDatabase } from "../utils/testDatabase";
 import { ShortURL } from "../../src/database/models/url";
 import URLShortnerService from "../../src/services/urlShortner.service";
 import NotUniqueStrategy from "../utils/mockStrategy";
+import { createDatabase } from "../utils/testDatabase";
 
 describe("URL Shortner service", () => {
   test("should save URL generated in the database", async () => {
+    const AppDataSource = await createDatabase();
     const strategy = new NotUniqueStrategy();
     const shortURLRepo = AppDataSource.getRepository(ShortURL);
     const service = new URLShortnerService(strategy, shortURLRepo);
@@ -19,12 +20,11 @@ describe("URL Shortner service", () => {
   });
 
   test("should throw uniqueness exception on repeated code", async () => {
+    const AppDataSource = await createDatabase();
     const strategy = new NotUniqueStrategy();
     const urlShortRepo = AppDataSource.getRepository(ShortURL);
     const service = new URLShortnerService(strategy, urlShortRepo);
-    expect(async () => {
-      await service.create("https://google.com");
-      await service.create("https://uol.com.br");
-    }).rejects.toThrow();
+    await service.create("https://google.com");
+    await expect(service.create("https://uol.com.br")).rejects.toThrow();
   });
 });
--- test/services/visit.service.spec.ts
+++ test/services/visit.service.spec.ts
@@ -1,12 +1,13 @@
-import { AppDataSource, startDatabase } from "../utils/testDatabase";
 import { ShortURL } from "../../src/database/models/url";
 import { Visit } from "../../src/database/models/visit";
 import URLShortnerService from "../../src/services/urlShortner.service";
 import VisitService from "../../src/services/visit.service";
 import NotUniqueStrategy from "../utils/mockStrategy";
+import { createDatabase } from "../utils/testDatabase";
 
 describe("Visit service", () => {
   test("should count the visit", async () => {
+    const AppDataSource = await createDatabase();
     const strategy = new NotUniqueStrategy();
     const shortURLRepo = AppDataSource.getRepository(ShortURL);
     const visitRepo = AppDataSource.getRepository(Visit);
