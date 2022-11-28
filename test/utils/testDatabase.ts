e745cd1e38c366a8d6eef0e3ac76f235a1052c02
Raphael D. Pinheiro <raphaklaus@gmail.com>
2022-11-28 13:18:27 +0100

    Refactor testing database utility

--- test/utils/testDatabase.ts
+++ test/utils/testDatabase.ts
@@ -1,22 +1,15 @@
 import { DataSource } from "typeorm";
 import models from "../../src/database/models/models";
 
-export const AppDataSource = new DataSource({
-  type: "sqlite",
-  database: ":memory:",
-  dropSchema: true,
-  entities: models,
-  synchronize: true,
-  logging: false,
-});
+export async function createDatabase() {
+  const AppDataSource = new DataSource({
+    type: "sqlite",
+    database: ":memory:",
+    dropSchema: true,
+    entities: models,
+    synchronize: true,
+    logging: false,
+  });
 
-export async function startDatabase() {
-  try {
-    await AppDataSource.initialize();
-    return AppDataSource;
-  } catch (error) {
-    console.log("Problem initializing database", error);
-  }
+  return await AppDataSource.initialize();
 }
-
-startDatabase();
