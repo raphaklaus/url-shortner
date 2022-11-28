a920c59276cd223be7d5f16e1f2d14e3d14935e0
Raphael D. Pinheiro <raphaklaus@gmail.com>
2022-11-28 14:48:16 +0100

    Introduce environment variables

--- src/database/client.ts
+++ src/database/client.ts
@@ -4,11 +4,11 @@ import models from "./models/models";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "postgres",
-  password: "postgres",
-  database: "url_shortner",
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || "5432"),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   entities: models,
   synchronize: false,
   logging: false,
